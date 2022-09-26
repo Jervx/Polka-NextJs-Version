0ede2c89fe8516b13fe48e1bded104c70840a973
Jervx <louellagracechua@gmail.com>
2022-09-26 14:36:39 +0800

    More themes

--- components/ThemeSelector.js
+++ components/ThemeSelector.js
@@ -7,6 +7,8 @@ const ThemeSelector = () => {
     { name: "light", type: "light" },
     { name: "dark", type: "dark" },
     { name: "cupcake", type: "light" },
+    { name: "bumblebee", type: "light" },
+    { name: "emerald", type: "light" },
     { name: "halloween", type: "dark" },
     { name: "bumblebee", type: "light" },
     { name: "black", type: "dark" },
@@ -15,13 +17,19 @@ const ThemeSelector = () => {
     { name: "valentine", type: "light" },
     { name: "dracula", type: "dark" },
     { name: "fantasy", type: "light" },
+    { name: "synthwave", type: "dark" },
     { name: "lofi", type: "light" },
+    { name: "retro", type: "light" },
+    { name: "cyberpunk", type : "light"},
     { name: "coffee", type: "dark" },
     { name: "pastel", type: "light" },
+    { name: "garden", type: "light" },
     { name: "acid", type: "light" },
     { name: "wireframe", type: "light" },
     { name: "cmyk", type: "light" },
     { name: "autumn", type: "light" },
+    { name: "winter", type: "light" },
+    { name: "lemonade", type: "light" }
   ];
   const [selected, setSelected] = useState();
 
