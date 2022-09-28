1a976669a2eb700a3481735da482bcfa970dc914
Jervx <louellagracechua@gmail.com>
2022-09-28 22:43:10 +0800

    Debug Console

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,43 +15,58 @@ export const authOptions = {
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      const userExist = await Users.findOne({ email: user.email });
-      if (!userExist) {
-        const createUserData = await Users.create({
-          email: profile.email, 
-          name: user.name, 
-          givenName: profile.given_name, 
-          picture: user.image,
-          locale: "en-US",
-          provider: account.provider,
-          signInCount: 1,
-        });
-      } else {
-        const updateUserData = await Users.updateOne(
-          { email: user.email },
-          {
-            $set: {
-              lastSignedIn: new Date(),
-              picture : user.image,
-              name: user.name, 
-              givenName: profile.given_name
-            },
-            $inc: {
-              signInCount: 1,
-            },
-          }
-        );
+      try {
+        const userExist = await Users.findOne({ email: user.email });
+        if (!userExist) {
+          const createUserData = await Users.create({
+            email: profile.email,
+            name: user.name,
+            givenName: profile.given_name,
+            picture: user.image,
+            locale: "en-US",
+            provider: account.provider,
+            signInCount: 1,
+          });
+        } else {
+          const updateUserData = await Users.updateOne(
+            { email: user.email },
+            {
+              $set: {
+                lastSignedIn: new Date(),
+                picture: user.image,
+                name: user.name,
+                givenName: profile.given_name,
+              },
+              $inc: {
+                signInCount: 1,
+              },
+            }
+          );
+        }
+      } catch (e) {
+        console.error("Err signIn callback ", e);
       }
 
       return true;
     },
 
     async session({ session, token, user }) {
-        const uid = await Users.findOne({email : session.user.email}, {_id : 1});
-        session.user.uid = uid._id
-        session.accessToken = token.accessToken
-        return session
+      try {
+        const uid = await Users.findOne(
+          { email: session.user.email },
+          { _id: 1 }
+        );
+
+        if (uid) {
+          session.user.uid = uid._id;
+          session.accessToken = token.accessToken;
+        }
+      } catch (e) {
+        console.error("Err Session callback ", e);
       }
+
+      return session;
+    },
   },
   secret: process.env.JWT_SECRET,
 };
